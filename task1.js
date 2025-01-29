dcd8e56973b6cbe7c0e071d9b0b914fb52fa4c74
Stanislav Sidorov <ssydorov@ukrgasbank.com>
2025-01-29 12:53:32 +0200

    done HW

--- task1.js
+++ task1.js
@@ -1,7 +1,25 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const usersList = document.querySelector(".usersList");
+
+addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+
+    data.forEach(user => {
+      const userItem = document.createElement("li");
+
+      userItem.textContent = user.name;
+      usersList.appendChild(userItem);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
--- task2.js
+++ task2.js
@@ -5,3 +5,29 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userNameInput = document.getElementById("userNameInput");
+const getUserButton = document.getElementById("getUserButton");
+const userCity = document.getElementById("userCity");
+
+getUserButton.addEventListener("click", async () => {
+  const userName = userNameInput.value;
+
+  if (!userName) {
+    return alert("Please enter a user name");
+  }
+
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    const user = data.find(user => user.name === userName);
+
+    if (user) {
+      userCity.textContent = user.address.city;
+    } else {
+      userCity.textContent = "User not found";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
